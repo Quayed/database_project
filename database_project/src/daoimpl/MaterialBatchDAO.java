5f4ac9f18221ba5a0d4337fb31526030e8a9dbcc
al <a@saloweb.dk>
2015-04-14 11:06:58 +0200

    added MaterialBatchDAO

---
+++ database_project/src/daoimpl/MaterialBatchDAO.java
@@ -0,0 +1,41 @@
+package daoimpl;
+
+import java.util.List;
+
+import daointerfaces.DALException;
+import daointerfaces.IMaterialBatchDAO;
+import dto.MaterialBatchDTO;
+
+public class MaterialBatchDAO implements IMaterialBatchDAO {
+
+	@Override
+	public MaterialBatchDTO getMaterialBatch(int mbID) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MaterialBatchDTO> getMaterialBatchList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MaterialBatchDTO> getMaterialBatchList(int materialID) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createMaterialBatch(MaterialBatchDTO materialbatch) throws DALException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateMaterialBatch(MaterialBatchDTO materialbatch) throws DALException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
