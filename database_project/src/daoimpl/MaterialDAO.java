a8523db4de5bb091c580830d55a5fad87d910a34
al <a@saloweb.dk>
2015-05-04 20:23:34 +0200

    materialDAO test and bugfix

--- database_project/src/daoimpl/MaterialDAO.java
+++ database_project/src/daoimpl/MaterialDAO.java
@@ -36,7 +36,7 @@ public class MaterialDAO implements IMaterialDAO {
 	public List<MaterialDTO> getMaterialList() throws DALException {
 		List<MaterialDTO> list = new ArrayList<MaterialDTO>();
 		try {
-			rs = Connector.doQuery("SELECT material_id, material_name FROM material");
+			rs = Connector.doQuery("SELECT material_id, material_name, provider FROM material");
 			while (rs.next()) {
 				list.add(new MaterialDTO(rs.getInt("material_id"), rs.getString("material_name"), rs.getString("provider")));
 			}
@@ -49,11 +49,11 @@ public class MaterialDAO implements IMaterialDAO {
 	@Override
 	public void createMaterial(MaterialDTO material) throws DALException {
 		try {
-			ps = Connector.prepare("INSERT INTO material(material_id, material_name, provider) VALUES (?, ?, ?);");
-			ps.setInt(1, material.getMaterialID());
-			ps.setString(2, material.getMaterialName());
-			ps.setString(3, material.getProvider());
+			ps = Connector.prepare("INSERT INTO material (material_name, provider) VALUES (?, ?);");
+			ps.setString(1, material.getMaterialName());
+			ps.setString(2, material.getProvider());
 			ps.execute();
+			material.setMaterialID(Connector.getLastInsert(ps));
 		} catch (SQLException e) {
 			throw new DALException(e);
 		}
@@ -62,7 +62,7 @@ public class MaterialDAO implements IMaterialDAO {
 	@Override
 	public void updateMaterial(MaterialDTO material) throws DALException {
 		try{
-			ps = Connector.prepare("UPDATE metarial SET material_name = ?, provider = ? WHERE material_id = ?");
+			ps = Connector.prepare("UPDATE material SET material_name = ?, provider = ? WHERE material_id = ?");
 			ps.setString(1, material.getMaterialName());
 			ps.setString(2, material.getProvider());
 			ps.setInt(3, material.getMaterialID());
--- database_project/src/test/TestMaterialDAO.java
+++ database_project/src/test/TestMaterialDAO.java
@@ -2,15 +2,88 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import connector.Connector;
+import daoimpl.MaterialDAO;
+import daointerfaces.DALException;
+import daointerfaces.IMaterialDAO;
+import dto.MaterialDTO;
 
 public class TestMaterialDAO {
 	
-	// how to implement: See OperatorDAO and FormulaDAO
+	private static int insertID;
+	
+	private static IMaterialDAO materialDAO;
+	private MaterialDTO materialDTO;
+	
+	@BeforeClass
+	public static void connect() {
+		
+		ConnectorTest.connect();
+		
+		materialDAO = new MaterialDAO();
+
+	}
+
+
+	@Test
+	public void getFormula() {
+		try {
+			materialDTO = materialDAO.getMaterial(1);
+		} catch (DALException e) {
+			fail(e.getMessage());
+		}
+		assertTrue(materialDTO != null);
+	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getFormulaList(){
+		try {
+			materialDAO.getMaterialList();
+		} catch (DALException e) {
+			fail(e.getMessage());		
+		}
+	}
+	
+	@Test
+	public void createUpdateFormula(){
+		materialDTO = new MaterialDTO(0, "Origano", "OrIgana A/S");
+		try {
+			materialDAO.createMaterial(materialDTO);
+			insertID = materialDTO.getMaterialID();
+		} catch (DALException e) {
+			fail(e.getMessage());
+		}
+		
+		try {
+			materialDTO = materialDAO.getMaterial(insertID);
+		} catch (DALException e) {
+			fail(e.getMessage());
+		}
+		
+		materialDTO.setMaterialName("Persille");
+		try {
+			materialDAO.updateMaterial(materialDTO);
+		} catch (DALException e) {
+			fail(e.getMessage());
+		}
+		
+		try {
+			assertEquals(materialDTO.getMaterialName(), materialDAO.getMaterial(insertID).getMaterialName());
+		} catch (DALException e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@AfterClass
+	public static void close() {
+		try {
+			Connector.doUpdate("DELETE FROM material WHERE material_id = "+insertID);
+		} catch (DALException e) {}
+		ConnectorTest.close();
 	}
 
 }
